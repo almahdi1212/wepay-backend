1d7c4975a6989e359699c9c063f41bf2f61fe314
Almahdi1212 <almahdiorath12345@gmail.com>
2025-11-07 15:26:53 +0200

    add health route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// routes/web.php
+Route::get('/_health', function () {
+    return response('Laravel OK', 200);
+});
